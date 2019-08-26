ec5fc8c0481004ca573d3546e78699cf1f7f26f3
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-26 16:15:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end 
 
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(speakers)
-  speakers.collect {|speaker| badge_maker(speaker)}
+  speakers.collect {|speaker| badge_maker(name)}
+end
