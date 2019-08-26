c749f395ffc6cb161b1f95a2162f6278e7fdb109
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-26 16:31:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,3 +15,12 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def assign_rooms(speakers)
   speakers.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
 end
+
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+  puts badge
+ end
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
+end
