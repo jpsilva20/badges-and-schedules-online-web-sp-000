38ce30183a7a016ce6fbf453447619009c9d9cdf
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-26 16:15:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,7 @@ def badge_maker(name)
 end 
 
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def batch_badge_creator(speakers)
   speakers.collect {|speaker| badge_maker(name)}
 end
